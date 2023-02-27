f9c8d691f5df0fb5c687697d0d07103d7c3d5123
ocronis <sorokidzes@yandex.ru>
2023-02-27 11:50:47 +0300

    Sprint 6.1

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -80,13 +80,11 @@ class Comment(models.Model):
 class Follow(models.Model):
     user = models.ForeignKey(
         User,
-        on_delete=models.SET_NULL,
-        null=True,
+        on_delete=models.CASCADE,
         related_name='follower',
     )
     author = models.ForeignKey(
         User,
-        on_delete=models.SET_NULL,
-        null=True,
+        on_delete=models.CASCADE,
         related_name='following',
     )
