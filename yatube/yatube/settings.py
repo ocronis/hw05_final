93a26194a49f30210560de52db9f9a0544297e11
ocronis <sorokidzes@yandex.ru>
2023-04-24 13:14:42 +0300

    hide secret key

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@d7y02*_z8_i+y@&qhwqe4ylj%pj^a6g9lht1$f)_icoao+4oj'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
